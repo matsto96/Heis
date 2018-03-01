136d4a59031d38631d7246d328a15612969c8799
matsto96 <matssto@stud.ntnu.no>
2018-03-01 21:57:48 +0100

    Update lys.c

--- lys.c
+++ lys.c
@@ -15,7 +15,7 @@ void lysInne(void){
 		}
 	}
 }
-void etasjelysUteNed(void){
+void lysUteNed(void){
 	bestillingUteNed();
 	int i;
 	for(i = 0; i < N_FLOORS-1; i++){
@@ -27,7 +27,7 @@ void etasjelysUteNed(void){
 		}
 	}
 }
-void etasjelysUteOpp(void){
+void lysUteOpp(void){
 	bestillingUteOpp();
 	int i;
 	for(i = 0; i < N_FLOORS-1; i++){
@@ -39,7 +39,7 @@ void etasjelysUteOpp(void){
 		}
 	}
 }
-void etasjeIndikatorUte(void){
+void indikatorLysUte(void){
 	int etasje = elev_get_floor_sensor_signal();
 	if(etasje >= 0 && etasje < N_FLOORS){
 		elev_set_floor_indicator(etasje);
