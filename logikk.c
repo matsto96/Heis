8d5af774bef22962b2cfe0c78d81115d47759b6c
matsto96 <matssto@stud.ntnu.no>
2018-03-08 14:58:30 +0100

    Update logikk.c

--- logikk.c
+++ logikk.c
@@ -67,7 +67,7 @@ void startHeis(void){
 			break;
 		case STOPP:                         
 			for(i = 0; i < N_FLOORS; i++){
-				if(inneTabell[i] == 1){
+				if(tabellInne[i] == 1){
 					if(i < heisData.forrigeEtasje){
 						nesteTilstand = HEIS_NED;
 						break;
@@ -125,12 +125,12 @@ void startHeis(void){
 				heisData.doerAapenTidTaker = time(NULL);
 				elev_set_motor_direction(DIRN_STOP);
 				if(heisData.forrigeEtasje < N_FLOORS && heisData.forrigeEtasje > 0){
-					uteNedTabell[heisData.forrigeEtasje-1] = 0;
+					tabellNed[heisData.forrigeEtasje-1] = 0;
 				}
 				if(heisData.forrigeEtasje < N_FLOORS-1 && heisData.forrigeEtasje >= 0){
-					uteOppTabell[heisData.forrigeEtasje] = 0;
+					tabellOpp[heisData.forrigeEtasje] = 0;
 				}
-				inneTabell[heisData.forrigeEtasje] = 0;
+				tabellInne[heisData.forrigeEtasje] = 0;
 			}
 			else if(elev_get_obstruction_signal()){
 				naaTilstand = OBSTRUKSJON;
@@ -160,11 +160,11 @@ void startHeis(void){
 			stoppLys(1);
 			if(heisData.stopp == 0){   //Sletter ikke bestillinger fått etter første stopp..! 
 				for(i = 0; i < N_FLOORS; i++){
-					inneTabell[i] = 0;
+					tabellInne[i] = 0;
 				}
 				for(i = 0; i < N_FLOORS-1; i++){
-					uteNedTabell[i] = 0;
-					uteOppTabell[i] = 0;
+					tabellNed[i] = 0;
+					tabellOpp[i] = 0;
 				}
 				heisData.stopp = 1;
 			}
